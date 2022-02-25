123dff5e7a824535c6b6fe3af9f5ad83d295649d
Brooklin <brooklinjazzmyers@gmail.com>
2022-02-25 11:59:52 -0800

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Academy.MixProject do
   def project do
     [
       app: :academy,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
