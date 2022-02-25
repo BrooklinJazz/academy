d08b4d543bff4322476d5863ead0f641b5e1ce1f
Brooklin <brooklinjazzmyers@gmail.com>
2022-02-25 11:48:47 -0800

    add hex info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Academy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -18,11 +20,21 @@ defmodule Academy.MixProject do
     ]
   end
 
+  defp description() do
+    "Automated feedback for the academy"
+  end
+
+  def package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/BrooklinJazz/academy"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
